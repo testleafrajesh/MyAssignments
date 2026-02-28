12bb2e32122079161ddad53ff5d89bb073618be7
testleafrajesh <mail2rajeshwariv@gmail.com>
2026-02-28 19:23:20 +0530

    Add files via upload

---
+++ 01datatype.js
@@ -0,0 +1,13 @@
+var firstName = "Rajesh"
+console.log(typeof firstName)
+var companyName = "Testleaf"
+console.log(typeof companyName)
+var mobilenumber = 12345
+console.log(typeof mobilenumber)
+var isAutomation = true
+console.log(typeof isAutomation)
+var hasPlaywright
+console.log(typeof hasPlaywright)
+
+
+
