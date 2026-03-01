b969906e34e494663e9684b88a618ae617ea8423
Rajeshwari_PlayWright <mail2rajeshwariv@gmail.com>
2026-03-01 12:13:07 +0530

    Add files via upload

---
+++ primitive_datatype_homeassignment.js
@@ -0,0 +1,18 @@
+let userName = "Rajeshwari"
+console.log(typeof userName)
+console.log("Username: "+ userName)
+
+let organization = "Motorola"
+console.log(typeof organization)
+console.log("Organization: "+ organization )
+
+let contactNumber = 9696969696
+console.log(typeof contactNumber)
+console.log("Contact Number: "+ contactNumber)
+
+let knowsAutomation = false
+console.log(typeof knowsAutomation)
+console.log("Knows Automation: "+ knowsAutomation)
+
+let usesPlaywright
+console.log(typeof usesPlaywright)
